4414c4b8e4454119f863bfb8da5cc71e16736b54
teenrage-dev <72465313+teenrage-dev@users.noreply.github.com>
2022-06-13 11:56:40 +0000

    Deploying to gh-pages from @ teenrage-dev/practise-js@04a6e70e2d0e3b7dbf53505e291a1f84ab249cf4 🚀

---
+++ index.4c28c325.js
@@ -0,0 +1,2 @@
+console.log(function(o,e){for(let l=0;l<e;l+=1)o.push(o[l]+o[l+1]+o[l+2]);return o.slice(0,e)}([1,1,1],10));
+//# sourceMappingURL=index.4c28c325.js.map
---
+++ index.99d26462.js
@@ -0,0 +1,2 @@
+console.log(function(o,n){for(var r=0;r<n;r+=1)o.push(o[r]+o[r+1]+o[r+2]);return o.slice(0,n)}([1,1,1],10));
+//# sourceMappingURL=index.99d26462.js.map
